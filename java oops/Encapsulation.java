e928ee5d50e165caa7c17a09b101b3558475fb77
CHINTHA KAMESWARA LOKESH <84376848+lokeshchintha@users.noreply.github.com>
2023-11-11 08:54:36 +0530

    Update Encapsulation.java

--- java oops/Encapsulation.java
+++ java oops/Encapsulation.java
@@ -4,34 +4,70 @@ package lokesh;
  * we create setter and getter methods to set and get methods
  */
 
-public class Encapsulation {
-	
-	private String name = "lokesh";
-	private int age=20;
-	
-	public String getName() {
-		return name;
-	}
+public class Student {
+    private String name;
+    private int age;
+    private String course;
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    // Constructor
+    public Student(String name, int age, String course) {
+        this.name = name;
+        this.age = age;
+        this.course = course;
+    }
 
-	public int getAge() {
-		return age;
-	}
+    // Getter methods
+    public String getName() {
+        return name;
+    }
 
-	public void setAge(int age) {
-		this.age = age;
-	}
-	
-	public static void main(String args[]) {
-		Encapsulation e = new Encapsulation();
-		System.out.println(e.getName());// lokesh
-		e.setName("loke");
-		System.out.println(e.getName());// loke
-		System.out.println(e.getAge());// 20
-		e.setAge(19);
-		System.out.println(e.getAge());// 19
-	}
+    public int getAge() {
+        return age;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    // Setter methods
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        if (age > 0) {
+            this.age = age;
+        } else {
+            System.out.println("Invalid age. Age remains unchanged.");
+        }
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    // Display method
+    public void displayInfo() {
+        System.out.println("Student: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Course: " + course);
+    }
+
+    public static void main(String[] args) {
+        // Creating a Student object
+        Student student1 = new Student("Alice", 20, "Computer Science");
+
+        // Displaying initial information
+        System.out.println("Initial Student Information:");
+        student1.displayInfo();
+
+        // Performing operations on the student object
+        student1.setAge(21);
+        student1.setCourse("Information Technology");
+
+        // Displaying updated information
+        System.out.println("\nUpdated Student Information:");
+        student1.displayInfo();
+    }
 }
+
