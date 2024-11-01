8c180777439a66ac88da0339b6aeadaf3e603992
CHINTHA KAMESWARA LOKESH <84376848+lokeshchintha@users.noreply.github.com>
2024-11-01 18:11:47 +0530

    Update Threads.java

--- Extra/Threads.java
+++ Extra/Threads.java
@@ -106,3 +106,83 @@ class B implements Runnable{
     }
 }
 
+// we can create classes using lambda expressions and anonymous classes as Runnable is a fucntional interface
+
+class Testing{
+        public static void main(String[] args) {
+
+            // Anonymous classes
+            Runnable obj1 = new Runnable(){
+                @Override
+                public void run(){
+                    for (int i = 0; i < 10; i++) {
+                        System.out.println("Hi");
+                    }
+                }
+            };
+
+            // lambda expressions
+            Runnable obj2 = () ->{
+                    for (int i = 0; i < 10; i++) {
+                        System.out.println("Hello");
+                    }
+                };
+
+            Thread t1 = new Thread(obj1);
+            Thread t2 = new Thread(obj2);
+
+            t1.start();
+            t2.start();
+    
+        }
+    }
+
+// If we declare any method as synchronized then it will accessible by only one thread at a time
+
+// Thread.join
+
+// Thread states - > (New, -> when we create thread , when we use start it will go to runnable state
+// Runnable, -> waits until it starts execution
+// Running,  -> running in cpu
+// Waiting,  -> if we use sleep() it went to waiting state
+// Dead -> if we use stop() it will go to dead state
+// )
+
+class Testing{
+        public static void main(String[] args) throws InterruptedException {
+
+            // Anonymous classes
+            Runnable obj1 = new Runnable(){
+                @Override
+                public void run(){
+                    for (int i = 0; i < 100; i++) {
+                        System.out.println("Hi");
+                    }
+                }
+            };
+
+            // lambda expressions
+            Runnable obj2 = () ->{
+                    for (int i = 0; i < 100; i++) {
+                        System.out.println("Hello");
+                    }
+                };
+
+            Thread t1 = new Thread(obj1);
+            Thread t2 = new Thread(obj2);
+
+            t1.start();
+            t2.start();
+
+
+             System.out.println("Hellox"); // if we give like this it will execute simultaneously with t1 and t2 
+            //  but if we want to execute after completion of t1 and t2 we use join()
+
+             t1.join(); // it makes sure thread1 is completed after completion it go to next steps
+             t2.join(); // it makes sure thread2 is completed it also has Interrupted Exception
+
+             System.out.println("Hello1"); // It will execute after t1 and t2 completes its processes
+
+    
+        }
+    }
